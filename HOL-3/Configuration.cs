e1356e76f7c392f59949144b66f2c4bdc9341116
Keerthi Reddy <115027344+Keerthireddy369@users.noreply.github.com>
2022-11-30 09:26:19 +0000

    Hol-3

---
+++ HOL-3/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace HOL_3_MVC.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<HOL_3_MVC.Models.MyDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(HOL_3_MVC.Models.MyDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ HOL-3/MyDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace HOL_3_MVC.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext()
+
+            : base("name=conn")
+
+        {
+
+
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
